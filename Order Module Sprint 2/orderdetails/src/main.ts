ef06794d0725903e4aff05ac964edbd73e514427
HARIHARAN P <hariharan1122004@gmail.com>
2025-11-22 17:51:49 +0530

    Delete Order Module Sprint 2/orderdetails directory

--- Order Module Sprint 2/orderdetails/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { OrderDetailsModule } from './app/order-details.module';
-
-platformBrowserDynamic()
-  .bootstrapModule(OrderDetailsModule)
-  .catch((err) => console.error(err));
